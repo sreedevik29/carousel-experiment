19e1b1ce31084a18e8ef35d17e0d84325ae77337
Sreedevi Krishnan <sreedevik29@gmail.com>
2017-06-05 19:34:24 -0400

    Add basic carousel transition

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
-	$("#bg-color").click(function(){
-		$(this).css("background", "yellow");
-	});
 
+	// DOM Selectors
+	var main = $("#main");
+	
+	var windowHeight = $(window).height();	
+	$(".box").css("height", windowHeight);
+
+	main.on("click", function() {
+		main.css("margin-left", "-100%");
+	});
 });
+
+// When the document loads, 
+// you assign the window height to every box div
+	// find the function for window height
+	// select all the boxes
+	// set the height of the section (box) to the window height
